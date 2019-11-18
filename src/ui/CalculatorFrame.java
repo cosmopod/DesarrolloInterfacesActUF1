919539d1169a4b4495d95df1f06a6150384d106b
cosmopod <fraurerod@gmail.com>
2019-11-18 18:40:16 +0100

    added calculator display

--- src/ui/CalculatorFrame.java
+++ src/ui/CalculatorFrame.java
@@ -6,6 +6,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.MatteBorder;
+import java.awt.Color;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
 
 public class CalculatorFrame extends JFrame {
 
@@ -35,8 +46,34 @@ public class CalculatorFrame extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel DisplayPanel = new JPanel();
+		DisplayPanel.setBackground(new Color(240, 248, 255));
+		FlowLayout flowLayout = (FlowLayout) DisplayPanel.getLayout();
+		flowLayout.setHgap(10);
+		flowLayout.setAlignment(FlowLayout.RIGHT);
+		DisplayPanel.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(DisplayPanel, GroupLayout.PREFERRED_SIZE, 229, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(185, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(DisplayPanel, GroupLayout.PREFERRED_SIZE, 39, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(201, Short.MAX_VALUE))
+		);
+		
+		JLabel DisplayText = new JLabel("0");
+		DisplayText.setHorizontalAlignment(SwingConstants.RIGHT);
+		DisplayText.setFont(new Font("Arial", Font.BOLD, 20));
+		DisplayPanel.add(DisplayText);
+		contentPane.setLayout(gl_contentPane);
 	}
-
 }
